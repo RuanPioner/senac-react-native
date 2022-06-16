643ec273182d46217d4cccab29f6c859f17d6a3f
Ruan <guest01@alunos.sc.senac.br>
2022-06-15 21:14:52 -0300

    Desafio Typescript objeto

---
+++ Typescript/desafio1.ts
@@ -0,0 +1,15 @@
+interface Carrinho {
+    id:number
+    descricao:string
+    preco:number
+    qtde:number
+}
+
+var produtos: Carrinho = {id:0,descricao:'',preco:0,qtde:0}
+
+produtos.id = 1
+produtos.descricao = 'Mouse HyperX'
+produtos.preco = 347
+produtos.qtde = 1
+
+console.log(produtos)
